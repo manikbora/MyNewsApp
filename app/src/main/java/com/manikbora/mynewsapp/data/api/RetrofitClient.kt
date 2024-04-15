26ef72fe93b066a96723b0f6576f043c9a8d56b6
Manik Bora <manikbora@gmail.com>
2024-04-15 16:57:37 +0530

    Code Refactored

--- app/src/main/java/com/manikbora/mynewsapp/data/api/RetrofitClient.kt
+++ app/src/main/java/com/manikbora/mynewsapp/data/api/RetrofitClient.kt
@@ -1,11 +1,10 @@
 package com.manikbora.mynewsapp.data.api
 
-import com.google.gson.GsonBuilder
+import com.manikbora.mynewsapp.utils.Constants.Companion.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://newsapi.org/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
